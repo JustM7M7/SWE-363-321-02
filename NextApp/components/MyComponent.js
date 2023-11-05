62b1a563ca1f2d420a6c6ac16f84d22050078887
ItsJustM7 <mohammed54268@gmail.com>
2023-11-05 18:51:03 +0300

    JS5_Exercise_2

---
+++ NextApp/components/MyComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyComponent = () => {
+  return (
+    <div>
+      <h1>This is my Next.js component</h1>
+    </div>
+  );
+};
+
+export default MyComponent;
---
+++ NextApp/pages/index.js
@@ -0,0 +1,14 @@
+// pages/index.js
+import React from 'react';
+import MyComponent from '../components/MyComponent';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>My Simple Website</h1>
+      <MyComponent />
+    </div>
+  );
+};
+
+export default Home;
